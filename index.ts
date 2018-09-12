93f1a68bbfd1188fb7b11350b3032606d882a842
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2018-09-12 11:46:15 -0700

    export additional functions (#4)

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-export { ReadMeManipulator, SuppressionItem, hasSuppressionBlock } from "./readMeManipulator"
+export {
+    addSuppression,
+    base64ToTree,
+    stringToTree,
+    ReadMeManipulator,
+    SuppressionItem,
+    hasSuppressionBlock
+} from "./readMeManipulator"
 export { ReadMeBuilder } from "./readMeBuilder"
 export { base64ToString, stringToBase64 } from "./gitHubUtils"
